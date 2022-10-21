8bc670471d09e193e35ae6a96553a5d073b4963a
Pedro Henrique Lopes <phs@lionx.com.br>
2022-10-20 23:50:37 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import { Envelope } from "phosphor-react";
+import { Heading } from "./components/Heading";
+import { Text } from "./components/Text";
+import { TextInput } from "./components/TextInput";
+import { Logo } from "./Logo";
 import "./styles/global.css";
 
 export function App() {
   return (
-    <>
-      <h1 className="font-bold text-2xl text-violet-500">Hello World!</h1>
+    <div className="w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100">
+      <header className="flex flex-col items-center">
+        <Logo />
 
-      <button className="bg-cyan-500 font-medium px-4 py-2 text-white hover:bg-cyan-300 ">
-        Enviar
-      </button>
-    </>
+        <Heading size="lg" className="mt-4">
+          Ignite Lab
+        </Heading>
+
+        <Text size="lg" className="text-gray-400 mt-1">
+          Faça login e comece a usar!
+        </Text>
+      </header>
+
+      <form className="flex flex-col items-stretch">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+
+            <TextInput.Input placeholder="Digite seu e-mail" />
+          </TextInput.Root>
+        </label>
+      </form>
+    </div>
   );
 }
